f0088e4fd0b429a1245582a761b851de55f48617
Mattia <mattia.soprano@gmail.com>
2016-07-07 11:13:59 +0200

    seriale finito+

--- progetto/seriale/src/main.cpp
+++ progetto/seriale/src/main.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 #include"classi.hpp"
+#include<fstream>
+#include<stdlib.h>
 
 using namespace std;
 
 int main()
 {
     MATRIX data("problem.csv");
+
+    vector<int> iteration;
+     //iterazioni
+
+    int value;
+
+    //apertura e controllo file
+    string input("problem.csv");
+    ifstream f(input);
+    string s;
+    if(!f){//check file giusto
+
+            cout<<"file non esiste"<<endl;
+            return 0;
+    }
+
+    //numero iterazioni
+    getline(f,s);
+    for(auto it=s.begin();it<s.end();it++){//iterazioni
+            value=atoi(&(*it));
+            iteration.push_back(value);
+            while((*it)!=','&& it<s.end()) it++;
+    }
+    f.close();
     int i=0;
-    while (i<1){
-    data.update();
+    auto N=iteration.size();
+    for(auto j=0;j<N;j++){
+        int N_it=iteration[j];
+        while (i<N_it){
+            data.update();
             i++;
+        }
+        data.print(i);
     }
-    cout<<"iterazione "<<i<<endl;
-    data.print();
-    data.print(i);
-    while (i<10){
-    data.update();
-    i++;
-    }
-    cout<<"iterazione "<<i<<endl;
-    data.print();
-    data.print(i);
-    while (i<100){
-    data.update();
-    i++;
-    }
-    cout<<"iterazione "<<i<<endl;
-    data.print();
-    data.print(i);
+
     return 0;
 }
