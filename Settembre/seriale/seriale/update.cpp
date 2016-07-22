53240ee55ddc709ba71fcbe0df52e6ac05d99cb5
Mattia <mattia.soprano@gmail.com>
2016-07-23 01:06:51 +0200

    sistemato blue manca controllo ultima riga e aggiunta

--- Settembre/seriale/seriale/update.cpp
+++ Settembre/seriale/seriale/update.cpp
@@ -9,6 +9,7 @@ bool ComparePoint(point const & p1, point const & p2){
     if(p1.row<p2.row){
         return true;
     }else{
+        if(p1.row>p2.row) {return false;}
         if(p1.col<p2.col){
             return true;
         }
@@ -18,35 +19,38 @@ bool ComparePoint(point const & p1, point const & p2){
 
 
 void matrix::update(int iteration){
-    auto N=data.size();
     for(auto iter=0;iter<iteration;iter++){
         if(move_blue){
-            auto it=data.begin();
+            auto start=data.begin();
             auto fine=data.end();
             vector<vector<point>::iterator> erase;
             vector<point> add;
+            int dim=sizeof(point);
             //controllo
-            for(size_t i=0;i<N;i++){
-                if(data[i].car==1){
-                    point p=data[i];
-                    point aux(1,p.row+1,p.col);
-                    if(p.row==N_row-1){ aux.row=0;}
-                    it=lower_bound(it,fine,aux,ComparePoint);
-                    if(it->col!=p.col && it!=fine){
+            for(auto p=data.begin();p!=fine;p++){
+                if(p->car==1){
+                    point punto=(*p);
+                    point aux(1,punto.row+1,punto.col);
+                    if(punto.row==N_row-1){
+                        aux.row=0;}
+                    else{
+                        if(!binary_search(p,fine,aux,ComparePoint)){
                         add.push_back(aux);
-                        erase.push_back(it);
-                        cout<<it->row<< " "<<it->col<< " eliminazione"<<endl;                    }
+                        erase.push_back(p);
+                        }
+                    }
                 }
             }
 
             //cancellazzione
             auto N_change=erase.size();
             for(int i=N_change-1;i>=0;i--){
+                cout<<"cancellazione "<<i<<" su "<<N_change<<endl;
                 data.erase(erase[i]);
 
             };
             //aggiunta
-            it=data.begin();
+            auto it=data.begin();
             bool first=true;
             for(size_t i_add=0;i_add<N_change;i_add++){
                 auto p=add[i_add];
