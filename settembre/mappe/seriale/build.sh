242f714b490d19fd747f04dd3222e7bb37feb087
Mattia <mattia.soprano@gmail.com>
2016-08-31 11:09:16 +0200

    progetto seriale con mappe finito e funzionante

--- settembre/mappe/seriale/build.sh
+++ settembre/mappe/seriale/build.sh
@@ -3,6 +3,6 @@
 g++ -c -std=c++11 src/main.cpp -I./include
 g++ -c -std=c++11 src/print.cpp -I./include
 g++ -c -std=c++11 src/read.cpp -I./include
-g++ -c -std=c++11 -Wall src/update.cpp -I./include
+g++ -c -std=c++11 src/update.cpp -I./include
 g++ -o seriale main.o print.o read.o update.o
 rm *.o
